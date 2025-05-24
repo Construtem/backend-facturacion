771974c9227c2470c3974a591c646edf6aa43c83
Diego Poblete <dpobletes@utem.cl>
2025-05-24 18:40:07 -0400

    main.go con las migraciones

--- main.go
+++ main.go
@@ -1,16 +1,81 @@
 package main
 
-import "fmt"
-import "github.com/gin-gonic/gin"
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+// Modelos
+type Cliente struct {
+	ID        uint `gorm:"primaryKey"`
+	Nombre    string
+	Correo    string
+	Telefono  string
+	Direccion string
+	Facturas  []Factura
+}
+
+type Factura struct {
+	ID        uint `gorm:"primaryKey"`
+	ClienteID uint
+	Cliente   Cliente
+	Fecha     string
+	Total     float64
+	Detalles  []DetalleFactura
+	Pagos     []Pago
+}
+
+type DetalleFactura struct {
+	ID        uint `gorm:"primaryKey"`
+	FacturaID uint
+	Producto  string
+	Cantidad  int
+	Precio    float64
+}
+
+type Pago struct {
+	ID        uint `gorm:"primaryKey"`
+	FacturaID uint
+	Monto     float64
+	FechaPago string
+	Metodo    string
+}
+
+var db *gorm.DB
+
+func initDatabase() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error cargando .env")
+	}
+
+	dsn := os.Getenv("DATABASE_URL")
+	db, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		log.Fatal("Error conectando a la base de datos:", err)
+	}
+
+	err = db.AutoMigrate(&Cliente{}, &Factura{}, &DetalleFactura{}, &Pago{})
+	if err != nil {
+		log.Fatal("Error en la migración de esquemas:", err)
+	}
+	fmt.Println("Migraciones realizadas con éxito")
+}
 
 func main() {
-	fmt.Println("Corriendo en localhost:8080 !!!")
-	
-  	router := gin.Default()
-  	router.GET("/", func(c *gin.Context) {
-  	  	c.JSON(200, gin.H{
-  	    	"message": "pong",
-  	  	})
-  	})
-  	router.Run() // listen and serve on 0.0.0.0:8080
+	initDatabase()
+
+	router := gin.Default()
+
+	router.GET("/", func(c *gin.Context) {
+		c.JSON(200, gin.H{"message": "pong"})
+	})
+
+	router.Run(":8080")
 }
