d3ea62ddb6d49a0b0adc02d02d916004d0003250
Manuxxrsb <Manuxxrsb@gmail.com>
2025-05-28 20:18:26 -0400

    Refactorizar el archivo main.go para utilizar un enrutador separado y agregar la configuración de rutas en routes/routes.go

--- main.go
+++ main.go
@@ -1,16 +1,14 @@
 package main
 
-import "fmt"
-import "github.com/gin-gonic/gin"
+import (
+	"backend-facturacion/routes"
+	"fmt"
+)
 
 func main() {
 	fmt.Println("Corriendo en localhost:8080 !!!")
 	
-  	router := gin.Default()
-  	router.GET("/", func(c *gin.Context) {
-  	  	c.JSON(200, gin.H{
-  	    	"message": "pong",
-  	  	})
-  	})
+	router := routes.SetupRouter()
   	router.Run() // listen and serve on 0.0.0.0:8080
+	
 }
